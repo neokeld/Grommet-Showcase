de6eeb05698074d6e762ed08afcb9ba59d7a77ec
neokeld <arnaud.duforat@capgemini.com>
2020-01-31 17:42:48 +0100

    Add Listbox, Messages and Progressbar components showcases

---
+++ src/showcase/MessagesPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Box, Button, Collapsible, Heading, Text } from "grommet";
+
+export const MessagesPage = props => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+      <Box pad="large">
+        <Heading>Messages</Heading>
+        <Button primary onClick={() => setOpen(!open)} label="Toggle" />
+        <Collapsible open={open} {...props}>
+          <Box
+            background="light-2"
+            round="medium"
+            pad="medium"
+            align="center"
+            justify="center"
+          >
+            <Text>This is a box inside a Collapsible component</Text>
+          </Box>
+        </Collapsible>
+        <Text>This is other content outside the Collapsible box</Text>
+      </Box>
+  );
+};
