d28c8f22f9b1d435d92961aca84afaa4edfecf6c
neokeld <arnaud.duforat@protonmail.ch>
2020-03-21 10:44:07 +0100

    Extract Panel component

--- src/showcase/PanelPage.js
+++ src/showcase/PanelPage.js
@@ -1,34 +1,16 @@
 import React from "react";
 
-import { Box, Button, Collapsible, Header, Heading, Text } from "grommet";
+import { Box, Heading, Text } from "grommet";
 
-export const PanelPage = props => {
-  const [open, setOpen] = React.useState(false);
+import { Panel } from "../lib/Panel";
 
-  return (
+export const PanelPage = props => (
       <Box pad="large">
 	<Heading>Panel</Heading>
-	<Header
-	  background="dark-3"
-	  pad="small">
-          <Text>Title</Text>
-	  <Button
-	    primary={!open}
-	    onClick={() => setOpen(!open)}
-	    label="Toggle" />
-        </Header>
-	<Collapsible open={open} {...props}>
-          <Box
-            background="light-3"
-            pad="medium"
-            align="center"
-            justify="center"
-          >
-            <Text>Panel is a grouping component providing with content toggle feature.</Text>
-	    <Text>Here this is done with a box inside a Collapsible component.</Text>
-          </Box>
-        </Collapsible>
+	<Panel title="Title">
+          <Text>Panel is a grouping component providing with content toggle feature.</Text>
+	  <Text>Here this is done with a box inside a Collapsible component.</Text>
+	</Panel>
       </Box>
-  );
-};
+);
 
