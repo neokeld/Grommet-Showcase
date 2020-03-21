000c2186e6862f7ab720033805c817e2b5cd77af
neokeld <arnaud.duforat@protonmail.ch>
2020-03-21 12:16:27 +0100

    Fix inline calendar example

--- src/showcase/CalendarPage.js
+++ src/showcase/CalendarPage.js
@@ -1,18 +1,6 @@
 import React from "react";
-import {
-  Box,
-  Calendar,
-  DropButton,
-  Heading
-} from "grommet";
+import { Box, Calendar, DropButton, Heading } from "grommet";
 import { Schedule } from "grommet-icons";
-
-const DropContent = ({ date: initialDate, onClose }) => (
-      <Calendar
-        date={initialDate}
-        onSelect={onClose}
-      />
-);
 
 export const CalendarPage = () => {
   const [date, setDate] = React.useState();
@@ -27,24 +15,21 @@ export const CalendarPage = () => {
       <Box pad="large">
 	<Heading>Basic Calendar</Heading>
 	<Box width="small">
-        <DropButton
-          open={open}
-          onClose={() => setOpen(false)}
-          onOpen={() => setOpen(true)}
-          dropContent={
-            <DropContent date={date} onClose={onClose} />
-          }
-	  label={date
-            ? new Date(date).toLocaleDateString()
-            : "Select date"}
-          icon={<Schedule />}
-	  reverse={true}
-	  hoverIndicator={true}
-        />
+          <DropButton
+            open={open}
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            dropContent={<Calendar date={date} onSelect={onClose} />}
+	    label={date
+              ? new Date(date).toLocaleDateString()
+              : "Select date"}
+            icon={<Schedule />}
+	    reverse={true}
+	    hoverIndicator={true}
+          />
 	</Box>
 	<Heading>Inline Calendar</Heading>
-	<Calendar date={new Date().toLocaleDateString()} />
+	<Calendar date={new Date().toISOString()} />
       </Box>
   );
 };
-
