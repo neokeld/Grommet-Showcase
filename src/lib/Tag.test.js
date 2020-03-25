6d99c2146ef3093491782dd999a60e79c1d54336
Neokeld <arnaud.duforat@protonmail.ch>
2020-03-25 17:37:37 +0100

    Unit tests on Tag

---
+++ src/lib/Tag.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Tag } from './Tag';
+
+test('renders even without onRemove', () => {
+  const wrapper = shallow(<Tag>Text</Tag>);
+  const textElement = 'Text';
+  expect(wrapper.contains(textElement)).toEqual(true);
+});
+
+test('renders a button with onRemove', () => {
+  const wrapper = shallow(<Tag onRemove={()=>{}}>Remove</Tag>);
+  expect(wrapper.find('Button')).toHaveLength(1);
+});
+
