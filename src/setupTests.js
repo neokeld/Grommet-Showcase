7b15c17fdb3ce85cd535016723c2538e4dc5c391
Neokeld <arnaud.duforat@protonmail.ch>
2020-03-25 17:42:34 +0100

    Add Enzyme testing utilities

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+//As of Enzyme 3, you will need to install Enzyme along with an Adapter corresponding to the version of React you are using.
+//The adapter will also need to be configured in your global setup file (here).
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
